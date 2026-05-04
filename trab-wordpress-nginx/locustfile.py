5f2fba10a876dc800f6dfcb1a369652ed84209f0
Icaro2M <icaromolina433@gmail.com>
2026-05-04 20:58:14 -0300

    ajustes testes

--- trab-wordpress-nginx/locustfile.py
+++ trab-wordpress-nginx/locustfile.py
@@ -3,23 +3,19 @@ from locust import HttpUser, task, between
 class UsuarioWordPress(HttpUser):
     wait_time = between(1, 3)
 
-
-    @task
-    def post_imagem_grande(self):
-        self.client.get("/?p=5", name="Cenario 1 - Imagem 1MB")
-
-
-    '''
-    @task
-    def post_texto(self):
-        self.client.get("/?p=12", name="Cenario 2 - Texto 500kb")
-    '''
-    
+   
     @task
     def post_imagem_media(self):
-        self.client.get("/?p=9", name="Cenario 3 - Imagem 300kb")
+        self.client.get("/?p=14", name="Cenario 1 - 100kb")
+     
+     
+    @task
+    def post_imagem_grande(self):
+        self.client.get("/?p=9", name="Cenario 2 - 400kb")
 
-
+  
     @task
     def post_imagem_maior(self):
-        self.client.get("/?p=14", name="Cenario 4 - Imagens 10mb")
+        self.client.get("/?p=5", name="Cenario 3 - 900kb")
+ 
+        
